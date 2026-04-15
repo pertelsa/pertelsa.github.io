86b798214cd5eafa20a067080481b28639024283
pertelsa <alpertelson@gmail.com>
2026-04-15 13:03:54 +0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,86 @@
+let map = L.map('map').setView([58.373523, 26.716045], 12);
+
+const esri_world= L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+});
+esri_world.addTo(map)
+
+
+// add geoJSON polygons layer*
+async function addDistrictsGeoJson(url) {
+  const response = await fetch(url)
+  const data = await response.json()
+  const polygons = L.geoJson(data, {
+    onEachFeature: popUPinfo,
+    style: polygonStyle
+  })
+  polygons.addTo(map)
+}
+
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+
+// add popup to each feature
+function popUPinfo(feature, layer) {
+  layer.bindPopup(feature.properties.NIMI)
+}
+
+// get color from feature property
+function getColor(property) {
+  switch (property) {
+    case 1:
+      return '#ff0000'
+    case 13:
+      return '#009933'
+    case 6:
+      return '#0000ff'
+    case 7:
+      return '#ff0066'
+    default:
+      return '#ffffff'
+  }
+}
+
+// polygon style
+function polygonStyle(feature) {
+  return {
+    fillColor: getColor(feature.properties.OBJECTID),
+    fillOpacity: 0.5,
+    weight: 1,
+    opacity: 1,
+    color: 'grey',
+  }
+}
+
+
+
+// add geoJSON points layer*
+async function addCelltowersGeoJson(url) {
+  const response = await fetch(url)
+  const data = await response.json()
+  const markers = L.geoJson(data)
+  markers.addTo(map)
+}
+addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+
+function createCircle(feature, latlng) {
+  let options = {
+    radius: 5,
+    fillColor: 'red',
+    fillOpacity: 0.5,
+    color: 'red',
+    weight: 1,
+    opacity: 1,
+  }
+  return L.circleMarker(latlng, options)
+}
+
+async function addCelltowersGeoJson(url) {
+  const response = await fetch(url)
+  const data = await response.json()
+  const circles = L.geoJson(data, {
+    pointToLayer: createCircle,
+  })
+  circles.addTo(map)
+}
+
+
