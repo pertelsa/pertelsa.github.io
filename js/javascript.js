bb3778f9dba7b5855d4b3db852918b2e76fe9dc4
pertelsa <alpertelson@gmail.com>
2026-04-15 12:39:34 +0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,29 @@
+let map = L.map('map').setView([58.373523, 26.716045], 12);
+
+const esri_world= L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+});
+esri_world.addTo(map)
+
+
+// add geoJSON polygons layer*
+async function addDistrictsGeoJson(url) {
+  const response = await fetch(url)
+  const data = await response.json()
+  const polygons = L.geoJson(data, {
+    onEachFeature: popUPinfo
+  })
+  polygons.addTo(map)
+}
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+
+
+// add popup to each feature
+function popUPinfo(feature, layer) {
+  layer.bindPopup(feature.properties.NIMI)
+}
+
+
+
+
+
