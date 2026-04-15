bb089e98ed9b45aa0b450650ba564f21ad42d948
pertelsa <alpertelson@gmail.com>
2026-04-15 12:59:49 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -51,6 +51,16 @@ function polygonStyle(feature) {
   }
 }
 
+// add geoJSON points layer*
+async function addCelltowersGeoJson(url) {
+  const response = await fetch(url)
+  const data = await response.json()
+  const markers = L.geoJson(data)
+  markers.addTo(map)
+}
+addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+
+
 
 
 
